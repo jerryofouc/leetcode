3c1b806ce0e441f6c5558b46c72891cece9e7076
张晓杰 <hzzhangxiaojie@corp.netease.com>
2014-02-27 17:17:08 +0800

    add cloneGraph

---
+++ src/main/java/jerryofouc/github/io/CloneGraph.java
@@ -0,0 +1,52 @@
+package jerryofouc.github.io;
+
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangxiaojie
+ * Date: 2/27/14
+ * Time: 16:28
+ * To change this template use File | Settings | File Templates.
+ */
+public class CloneGraph {
+    class UndirectedGraphNode {
+             int label;
+             ArrayList<UndirectedGraphNode> neighbors;
+             UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+    };
+
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if(node == null){
+            return null;
+        }
+        Map<Integer,UndirectedGraphNode> hasBuildMap = new HashMap<Integer, UndirectedGraphNode>();
+        Set<Integer> hasVisitSet = new HashSet<Integer>();
+        Stack<UndirectedGraphNode> stack = new Stack<UndirectedGraphNode>();
+        UndirectedGraphNode head = new UndirectedGraphNode(node.label);
+        hasBuildMap.put(head.label,head);
+        stack.push(node);
+        while(!stack.isEmpty()){
+            UndirectedGraphNode u = stack.pop();
+            hasVisitSet.add(u.label);
+            UndirectedGraphNode uc = hasBuildMap.get(u.label);
+            if(u.neighbors != null){
+                for(UndirectedGraphNode n : u.neighbors){
+                    UndirectedGraphNode c = hasBuildMap.get(n.label);
+                    if(c == null){
+                        c = new UndirectedGraphNode(n.label);
+                        hasBuildMap.put(n.label,c);
+                    }
+                    uc.neighbors.add(c);
+                    if(!hasVisitSet.contains(n.label)&&!stack.contains(n)){
+                        stack.push(n);
+                    }
+                }
+            }
+
+        }
+        return head;
+    }
+
+
+}
