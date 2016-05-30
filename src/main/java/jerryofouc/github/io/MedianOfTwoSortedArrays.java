dff01573e0e5e8ba69d1cfd01c820eb421239d18
zhangxiaojie <xiaojieaa@gmail.com>
2016-05-30 20:17:27 +0800

    add

--- src/main/java/jerryofouc/github/io/MedianOfTwoSortedArrays.java
+++ src/main/java/jerryofouc/github/io/MedianOfTwoSortedArrays.java
@@ -8,11 +8,90 @@ package jerryofouc.github.io;
  * To change this template use File | Settings | File Templates.
  */
 public class MedianOfTwoSortedArrays {
-    public double findMedianSortedArrays(int A[], int B[]) {
-        return findMedianSortedArrays(A,0,A.length,B,0,B.length);
+
+    public static void main(String args[]){
+        int A[] = {4};
+        int B[] = {1,2,3};
+        System.out.println(findMedianSortedArrays(A, B));
     }
 
-    private double findMedianSortedArrays(int[] a, int sa, int ea, int[] b, int sb, int eb) {
-        return 0;
+    public static double findMedianSortedArrays(int A[], int B[]) {
+
+        if (A.length > B.length) {
+            return findMedianSortedArrays(B, A);
+        }
+
+        if(A.length == 0){
+            if(B.length %2 == 0){
+                return (B[B.length/2] + B[B.length/2 - 1])/2.0;
+            }else {
+                return B[B.length/2];
+            }
+        }
+
+        int m = A.length;
+        int n= B.length;
+
+        int begin = 0;
+        int end = m-1;
+        int j = 0;
+        int i = (begin + end)/2;
+        while(begin <= end){
+            i = (begin + end)/2;
+            j = (m+n+1)/2 -i - 1;
+
+            if( A[i] > B[j]){
+                end = i-1;
+            }else if(i < m-1 &&  j >0 && A[i+1] < B[j-1] ){
+                begin = i+1;
+            }else {
+                if(j>0 && i<m-1 && A[i] <= B[j] && A[i+1]>=B[j-1] ){
+                    if((m+n) % 2 == 0){
+                        return ((Math.max(A[i],B[j-1])+Math.min(A[i+1],B[j]))/2.0);
+                    }else {
+                        return Math.max(A[i],B[j-1]);
+                    }
+                }else if(i == m-1){
+                    if((m+n) % 2 == 0){
+                        if(j>0){
+                            return ((Math.max(A[i],B[j-1])+B[j])/2.0);
+                        }else {
+                            return (A[i]+B[j])/2.0;
+                        }
+                    }else {
+                        return Math.max(A[i],B[j-1]);
+                    }
+                }else if(j == 0){
+                    if((m+n) % 2 == 0){
+                        return (A[m-1]+B[0])/2.0;
+                    }else {
+                        return B[0];
+                    }
+                }
+            }
+        }
+
+        if(end < 0){
+            if((m+n) % 2 == 0){
+                if(j == n-1){
+                    return (B[j] + A[i])/2.0;
+                }else {
+                    return (B[j] + Math.min(B[j+1],A[i]))/2.0;
+                }
+
+            }else {
+                return B[(m+n+1)/2-1];
+            }
+        }
+
+
+
+        return -1;
+
+
     }
+
+
+
+
 }
--- src/main/java/jerryofouc/github/io/ZigZagConversion.java
+++ src/main/java/jerryofouc/github/io/ZigZagConversion.java
@@ -6,4 +6,36 @@ package jerryofouc.github.io;
  *         功能介绍:
  */
 public class ZigZagConversion {
+
+    public static void main(String args[]){
+        System.out.println(convert("A",1));
+    }
+    public static String convert(String s, int nRows) {
+        if(nRows == 1){
+            return s;
+        }
+        char[] sc = s.toCharArray();
+        String result = "";
+        int interval = (nRows-1)*2 - 1;
+        for(int i=0;i<nRows;i++){
+            int start = i;
+            if(i == 0 || i == nRows-1){
+                while(start < s.length()){
+                    result += sc[start];
+                    start += interval+1;
+                }
+            }else {
+                int k = 0;
+                while(start < s.length()){
+                    result += sc[start];
+                    if(k++ %2 == 0){
+                        start += interval - i*2+1;
+                    }else {
+                        start += interval - (nRows - i-1)*2 + 1;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
