b0595adfa021848174146f8cc8c70bc16b70f6cf
xiaojiez <xiaojieaa@gmail.com>
2017-03-09 13:05:21 +0800

    add

---
+++ src/main/java/jerryofouc/github/io/FizzBuzz.java
@@ -0,0 +1,26 @@
+package jerryofouc.github.io;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xiaojiez on 3/9/17.
+ */
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0&&i%5==0){
+                result.add("FizzBuzz");
+            }else if(i%3==0){
+                result.add("Fizz");
+            }else if(i%5 == 0){
+                result.add("Buzz");
+            }else {
+                result.add(i+"");
+            }
+        }
+        return result;
+
+    }
+}
